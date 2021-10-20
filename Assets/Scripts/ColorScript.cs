f232ccdf696814fa413e0d3f6cdffb920afd2c20
Ivan Popenya <87383769+IvanPopik05@users.noreply.github.com>
2021-10-20 22:22:09 +0300

    First commit

---
+++ Assets/Scripts/ColorScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorScript : MonoBehaviour
+{
+    [SerializeField] Color[] colors;
+    private int counter;
+
+    public Color GetColor() 
+    {
+        return colors[counter++];
+    }
+    public Color RandomColor() 
+    {
+        int randColor = Random.Range(0,colors.Length);
+        return colors[randColor];
+    }
+}
